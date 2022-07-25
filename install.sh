1777fde0a3c12144496052bf9b038b821ba019b4
Joshua Ellis <67237931+SpicyTakis@users.noreply.github.com>
2022-07-25 12:18:09 -0400

    Update script

--- install.sh
+++ install.sh
@@ -1,1 +1,6 @@
+# install lts kernel, pacman, and networkmanager, and firmware
+pacstrap /mnt linux-lts pacman networkmanager linux-firmware bash
 
+arch-chroot /mnt /bin/bash << "EOT"
+systemctl enable NetworkManager
+EOT
